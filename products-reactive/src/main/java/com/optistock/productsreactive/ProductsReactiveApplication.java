ba3d769579320fe26585d9108002d8b495375156
ASUS1312\sadma <juanandreyquimbayo@gmail.com>
2024-11-09 11:24:19 -0500

    start proyect

---
+++ products-reactive/src/main/java/com/optistock/productsreactive/ProductsReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.optistock.productsreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsReactiveApplication.class, args);
+    }
+
+}
---
+++ products-reactive/src/test/java/com/optistock/productsreactive/ProductsReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optistock.productsreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
